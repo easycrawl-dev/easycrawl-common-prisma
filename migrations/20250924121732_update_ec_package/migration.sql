33c9b6acdc73baeb0c35efeb5fbc96b139c0aa23
senjianlu <admin@cs.cheap>
2025-09-24 20:18:09 +0800

    update: ec_package

---
+++ migrations/20250924121732_update_ec_package/migration.sql
@@ -0,0 +1,8 @@
+-- DropIndex
+DROP INDEX "public"."ec_package_invoiceId_key";
+
+-- DropIndex
+DROP INDEX "public"."ec_package_packageType_key";
+
+-- DropIndex
+DROP INDEX "public"."ec_package_userId_key";
