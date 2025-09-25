5cd9db108afa24ce80a8d04c0b7e5bac0e0ed476
Rabbir 兔子 <m18018230731@163.com>
2025-09-25 06:21:22 +0000

    feat: ec_task and ec_credit_transaction

---
+++ migrations/20250925062036_feat_ec_task_and_ec_credit_transaction/migration.sql
@@ -0,0 +1,26 @@
+-- CreateTable
+CREATE TABLE "public"."ec_task" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "urls" TEXT NOT NULL,
+    "status" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "completedAt" TIMESTAMP(3),
+    "isPublic" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "ec_task_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."ec_credit_transaction" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "taskId" TEXT NOT NULL,
+    "action" TEXT NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "description" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "ec_credit_transaction_pkey" PRIMARY KEY ("id")
+);
